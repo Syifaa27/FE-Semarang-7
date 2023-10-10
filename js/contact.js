9ebd451f186a962fe7b764e6410dd51b42659358
Syifa' Auliya <syifaauliyaas.15@student.uns.ac.id>
2023-10-10 18:35:39 +0700

    revisi  4

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
-$("#sendMsg").submit(function (event) {
-    event.preventDefault();
+    $("#sendMsg").submit(function (event) {
+        event.preventDefault();
 
-    var formData = $(this).serialize();
+        var formData = $(this).serialize();
 
-    $.post('https://be-semarang-7-production.up.railway.app/message', formData);
-});
+        $.post(
+            "https://be-semarang-7-production.up.railway.app/message",
+            formData
+        )
+            .done(function () {
+                alert(
+                    "Terima kasih atas pesannya! Pesan Anda telah berhasil dikirim"
+                );
+                location.reload();
+            })
+            .fail(function () {
+                alert("Maaf, terjadi kesalahan. Silahkan ulangi lagi!");
+            });
+    });
 });
